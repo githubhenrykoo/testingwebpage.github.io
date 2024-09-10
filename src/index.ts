dd213aaf5d0b90c04cfe50b611724128de2d5137
githubhenrykoo <lckoo1230@gmail.com>
2024-09-11 00:22:34 +0800

    add

---
+++ src/index.ts
@@ -0,0 +1,71 @@
+import { v4 as uuidV4 } from "uuid"
+
+type Task = {
+  id: string
+  title: string
+  completed: boolean
+  createdAt: Date
+}
+
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.getElementById("new-task-form") as HTMLFormElement | null
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
+const deleteCompletedButton = document.getElementById("delete-completed") as HTMLButtonElement | null
+const tasks: Task[] = loadTasks()
+tasks.forEach(addListItem)
+
+form?.addEventListener("submit", (e) => {
+  e.preventDefault()
+
+  if (input?.value === "" || input?.value == null) return
+
+  const newTask: Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  }
+  tasks.push(newTask)
+  saveTasks()
+
+  addListItem(newTask)
+  input.value = ""
+})
+
+deleteCompletedButton?.addEventListener("click", () => {
+  // Filter out completed tasks
+  const incompleteTasks = tasks.filter((task) => !task.completed)
+
+  // Clear the tasks array and push only the incomplete tasks
+  tasks.splice(0, tasks.length, ...incompleteTasks) // Modify the array in place
+  saveTasks()
+
+  // Re-render the task list
+  list!.innerHTML = "" // Clear the current task list UI
+  tasks.forEach(addListItem) // Rebuild the task list UI with incomplete tasks
+})
+
+function addListItem(task: Task) {
+  const item = document.createElement("li")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked
+    saveTasks()
+  })
+  checkbox.type = "checkbox"
+  checkbox.checked = task.completed
+  label.append(checkbox, task.title)
+  item.append(label)
+  list?.append(item)
+}
+
+function saveTasks() {
+  localStorage.setItem("TASKS", JSON.stringify(tasks))
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem("TASKS")
+  if (taskJSON == null) return []
+  return JSON.parse(taskJSON)
+}
